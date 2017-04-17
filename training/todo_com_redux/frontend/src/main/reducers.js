b4bcbe2e3885650c5a38c9071aac930d9306d737
doliveira <doliveira@cdts.fiocruz.br>
2017-04-17 10:36:19 -0300

    Configurando os Reducers (todo_com_redux\frontend)

---
+++ training/todo_com_redux/frontend/src/main/reducers.js
@@ -0,0 +1,24 @@
+import { combineReducers } from 'redux'
+
+
+const rootReducer  = combineReducers({
+    todo: () => ({
+        description: 'Ler livro', 
+        list: [{
+            _id: 1, 
+            description: 'Pagar fatura do cartão',
+            done:true
+        }, {
+            _id: 2, 
+            description: 'Reunião com equipe as 10:00hs',
+            done:false
+        }, {
+            _id: 1, 
+            description: 'Consulta médicas na terça depois do almoço',
+            done:false            
+        }]
+
+    })
+})
+
+export default rootReducer
